5d99650cf277522dc0a68cd9c5b538d02a78fba5
ferc <fernanda.corsanigo@gmail.com>
2026-02-11 10:47:43 -0300

    update error

--- src/utils/errorUtils.js
+++ src/utils/errorUtils.js
@@ -1,6 +1,7 @@
-export class ServerError extends Error{
-    constructor(message, status){
+export class ServerError extends Error {
+    constructor(message, status) {
         super(message)
         this.name = 'ServerError'
+        this.status = status
     }
 }
